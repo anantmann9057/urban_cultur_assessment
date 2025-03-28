0703f74114cdd7f854e59fb9c43b1f8ed78541c5
Anatn <anantmann9057@gmail.com>
2025-03-28 16:26:20 +0530

    streak screen work

--- lib/Presentation/Streaks/StreaksScreen.dart
+++ lib/Presentation/Streaks/StreaksScreen.dart
@@ -26,11 +26,86 @@ class _StreaksscreenState extends ConsumerState<Streaksscreen> {
       children: [
         ref.watch(streaksStream).when(
             data: (data) {
-              return Column(
-                children: [
-                  Text("Todays Goal: ${ref.watch(currentStreakProvider) + 1} Streak Days"),
-                  Text("Streaks: ${ref.watch(currentStreakProvider)}")
-                ],
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Todays Goal: ${ref.watch(currentStreakProvider) + 1} Streak Days",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Streak Days "),
+                              Text(
+                                "${ref.watch(currentStreakProvider)}",
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 18),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Daily Streak ",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "${ref.watch(currentStreakProvider)}",
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "last 30 Days - ${ref.watch(currentStreakProvider)>=1?'100%':"0%"}",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Image.asset('assets/images/graph.png'),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      "Keep it up! You're on a roll.",
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 16),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      width: MediaQuery.of(context).size.width,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          color: Colors.grey.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(8)),
+                      child: const Text('Get Started'),
+                    )
+                  ],
+                ),
               );
             },
             error: (e, s) => Container(),
@@ -47,14 +122,15 @@ class _StreaksscreenState extends ConsumerState<Streaksscreen> {
         var time = DateTime.fromMillisecondsSinceEpoch(
             (value.docs[i]['date'] as Timestamp).millisecondsSinceEpoch);
 
-        if (value.docs.length > 1 && i>0) {
+        if (value.docs.length > 1 && i > 0) {
           var previousTime = DateTime.fromMillisecondsSinceEpoch(
-              (value.docs[i-1]['date'] as Timestamp).millisecondsSinceEpoch);
+              (value.docs[i - 1]['date'] as Timestamp).millisecondsSinceEpoch);
 
           if (previousTime.difference(time).inDays > 1) {
             ref.watch(currentStreakProvider.notifier).state = 0;
           } else {
-            ref.watch(currentStreakProvider.notifier).state = value.docs.length-1;
+            ref.watch(currentStreakProvider.notifier).state =
+                value.docs.length - 1;
           }
         }
         if (time.day == DateTime.now().day) {}
